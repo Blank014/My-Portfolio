908f5b8a0b0e064f848e12c4765ddf379fd0fd51
Imad Eddin Alkuzbari <i.alkuzbari@tu-braunschweig.de>
2024-08-01 23:52:06 +0200

    added structure

---
+++ src/sections/About.js
@@ -0,0 +1,13 @@
+// src/sections/About.js
+import React from 'react';
+import './About.css';
+
+const About = () => (
+    <section id="about" className="about">
+        <h2>About Me</h2>
+        <p>I'm a [Your Profession] based in [Your Location]. I graduated from [Your University] with a degree in [Your Degree].</p>
+        <p>In my free time, I enjoy [Hobbies].</p>
+    </section>
+);
+
+export default About;
---
+++ src/sections/Home.js
@@ -0,0 +1,12 @@
+// src/sections/Home.js
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+    <section id="home" className="home">
+        <h1>Welcome to My Portfolio</h1>
+        <p>Hello! I'm [Your Name], a [Your Profession].</p>
+    </section>
+);
+
+export default Home;
